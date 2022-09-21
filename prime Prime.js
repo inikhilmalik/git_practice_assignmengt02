368d4383ae2483330f26ef7d889461cd30b34ee7
NIKHIL MALIK <inikhilmalik@gmail.com>
2022-09-21 21:56:50 +0530

    CodeUpdated

--- prime Prime.js
+++ prime Prime.js
@@ -1,14 +1,14 @@
 
 let n=13;
-let check=true;
-for(let i=2;i<n-1;i++)
+let factor=0;
+for(let i=1;i<n;i++)
   {
     if(n%i==0)
     {
-      check=false;
+      factor+=1;
     }
   }
-if(check==true)
+if(factor==2)
 {
   console.log("Prime");
 }
